85c72da2c94ebd6c1ddee347560d19de6178d9dd
Thomas Much <github@snailshell.de>
2020-11-12 13:42:55 +0100

    Extract base reader for monochrome images

---
+++ src/main/java/de/snailshell/imageio/MonochromeReader.java
@@ -0,0 +1,40 @@
+package de.snailshell.imageio;
+
+import javax.imageio.ImageReader;
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.spi.ImageReaderSpi;
+import java.awt.image.DataBuffer;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class MonochromeReader extends ImageReader {
+
+    protected MonochromeReader(ImageReaderSpi originatingProvider) {
+        super(originatingProvider);
+    }
+
+    @Override
+    public int getNumImages(boolean allowSearch) throws IOException {
+        return 1;
+    }
+
+    @Override
+    public Iterator<ImageTypeSpecifier> getImageTypes(int imageIndex) throws IOException {
+
+        var monochrome = ImageTypeSpecifier.createGrayscale(1, DataBuffer.TYPE_BYTE, false);
+
+        return List.of(monochrome).iterator();
+    }
+
+    @Override
+    public IIOMetadata getStreamMetadata() throws IOException {
+        return null;
+    }
+
+    @Override
+    public IIOMetadata getImageMetadata(int imageIndex) throws IOException {
+        return null;
+    }
+}
--- src/main/java/de/snailshell/imageio/pic/GEMPictureReader.java
+++ src/main/java/de/snailshell/imageio/pic/GEMPictureReader.java
@@ -1,28 +1,19 @@
 package de.snailshell.imageio.pic;
 
+import de.snailshell.imageio.MonochromeReader;
+
 import javax.imageio.ImageReadParam;
-import javax.imageio.ImageReader;
-import javax.imageio.ImageTypeSpecifier;
-import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.spi.ImageReaderSpi;
 import javax.imageio.stream.ImageInputStream;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
-public class GEMPictureReader extends ImageReader {
+public class GEMPictureReader extends MonochromeReader {
 
     static final String DESCRIPTION = "ImageIO reader for monochrome GEM 32000 bytes 640x400 *.PIC (*.DOO) images";
 
     GEMPictureReader(ImageReaderSpi originatingProvider) {
         super(originatingProvider);
-    }
-
-    @Override
-    public int getNumImages(boolean allowSearch) throws IOException {
-        return 1;
     }
 
     @Override
@@ -33,24 +24,6 @@ public class GEMPictureReader extends ImageReader {
     @Override
     public int getHeight(int imageIndex) throws IOException {
         return GEMPicture.HEIGHT;
-    }
-
-    @Override
-    public Iterator<ImageTypeSpecifier> getImageTypes(int imageIndex) throws IOException {
-
-        var monochrome = ImageTypeSpecifier.createGrayscale(1, DataBuffer.TYPE_BYTE, false);
-
-        return List.of(monochrome).iterator();
-    }
-
-    @Override
-    public IIOMetadata getStreamMetadata() throws IOException {
-        return null;
-    }
-
-    @Override
-    public IIOMetadata getImageMetadata(int imageIndex) throws IOException {
-        return null;
     }
 
     @Override
